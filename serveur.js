cbaf4abc0044326cd1c3fdc097bade84a8afa0b1
Ahmadzai Haseeb <68177125+ahmadzai2002@users.noreply.github.com>
2022-04-08 11:45:03 +0200

    Update serveur.js

--- serveur.js
+++ serveur.js
@@ -2,14 +2,23 @@ const http = require("http");
 const hostname = "0.0.0.0";
 const port = 8000;
 
+const routes = ["GET:/", "POST:/", "DELETE:/"];
 const server = http.createServer();
 
 server.on('request',(request, response) => {
-	response.writeHead(200, {'Content-type': 'text/plain'});
-	response.end('Hello World\n');
+        const { method, url, headers } = request;
+        const searchedRoute = `${method.toUpperCase()}:${url}`;
+        if(-1 < routes.findIndex((val) => { return val === searchedRoute; })) {  // nous  avons trouvé la route 
+                response.writeHead(200, {'Content-type': 'text/plain'});
+                response.end('Hello World\n');
+        } else {
+                response.writeHead(404);
+                response.end('Not Found\n');
+        }
 });
 
 server.listen(port, hostname, function() {
-	console.log(`Server running at http://${hostname}:${port}/`);
+        console.log(`Server running at http://${hostname}:${port}/`);
 
 })
+
